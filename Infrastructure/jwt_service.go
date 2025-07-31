a1d837c194ae557a4e2ac06f06ecd34ce287f2d2
Abreham Kassa <abrehamkassa19@gmail.com>
2025-07-31 11:46:18 +0300

    feat: update the token service interface definition

--- Infrastructure/jwt_service.go
+++ Infrastructure/jwt_service.go
@@ -3,6 +3,7 @@ package infrastructure
 type ITokenService interface {
 	GenerateToken() (string, error)
 	ValidateToken(string) error
+	RefreshToken(string) (string, error)
 }
 
 type JWTTokenSevice struct {
