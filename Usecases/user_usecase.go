fba7c6eaa83d17db52cc84f230a447edef904033
Abreham Kassa <abrehamkassa19@gmail.com>
2025-07-31 11:42:06 +0300

    feat: add login and authenticate methods for user usecases

--- Usecases/user_usecase.go
+++ Usecases/user_usecase.go
@@ -1,9 +1,35 @@
 package usecases
 
 import (
+	"errors"
+
 	"github.com/gedyzed/blog-starter-project/Domain"
+	"github.com/gedyzed/blog-starter-project/Infrastructure"
 )
 
 type UserUsecases struct {
-	repo domain.IUserRepository
+	repo         domain.IUserRepository
+	tokenService infrastructure.ITokenService
+}
+
+func (u *UserUsecases) Login(user domain.User) (string, error) {
+	data, err := u.repo.Get(user.Username)
+	if err != nil {
+		return "", errors.New("User does not exist")
+	}
+
+	if data.Password != user.Password || data.Email != user.Email {
+		return "", errors.New("invalid email or passwrod")
+	}
+
+	token, err := u.tokenService.GenerateToken()
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
+
+func (u *UserUsecases) Authenticate(token string) error {
+	return u.tokenService.ValidateToken(token)
 }
