efe1e36ebd36f1708e3ecd22d4474525e266e09a
Abreham Kassa <abrehamkassa19@gmail.com>
2025-07-31 12:04:35 +0300

    chore: move the token service interface to domain layer

--- Infrastructure/jwt_service.go
+++ Infrastructure/jwt_service.go
@@ -1,14 +1,6 @@
 package infrastructure
 
-import (
-	"github.com/gedyzed/blog-starter-project/Domain"
-)
-
-type ITokenService interface {
-	GenerateToken() (domain.Token, error)
-	ValidateToken(string) error
-	RefreshToken(string) (string, error)
-}
+import ()
 
 type JWTTokenSevice struct {
 }
