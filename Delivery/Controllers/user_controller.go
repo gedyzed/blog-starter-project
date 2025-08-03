a65a464ad7a392fbf69c63f0a8919acc169a57d2
Geda <gediizeyuu@gmail.com>
2025-08-03 23:29:05 +0300

    remove duplicate controllers

--- Delivery/Controllers/user_controller.go
+++ Delivery/Controllers/user_controller.go
@@ -2,11 +2,12 @@ package controllers
 
 import (
 	"net/http"
+	"fmt"
+	"regexp"
 
+	domain "github.com/gedyzed/blog-starter-project/Domain"
+	usecases "github.com/gedyzed/blog-starter-project/Usecases"
 	"github.com/gin-gonic/gin"
-
-	"github.com/gedyzed/blog-starter-project/Domain"
-	"github.com/gedyzed/blog-starter-project/Usecases"
 )
 
 type UserController struct {
@@ -61,31 +62,79 @@ func (uc *UserController) RegisterUser(c *gin.Context) {
 	// accepting user input
 	var user *domain.User
 	if err := c.ShouldBindJSON(&user); err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "invalid input format"})
+		c.IndentedJSON(400, gin.H{"error": "invalid input format"})
 		c.Abort()
 		return
 	}
 
 	// checking for required fields
-	if user.Email != "" || user.Username != "" || user.Password != "" || user.Firstname != "" {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "fill all required fields: email, username, password, firstname"})
+	if user.Email == "" || user.Username == "" || user.Password == "" || user.Firstname == "" {
+		c.IndentedJSON(400, gin.H{"error": "fill all required fields: email, username, password, firstname"})
 		c.Abort()
 		return
 	}
 
-	err := uc.userUsecase.Register(ctx, user)
+	// Basic validation
+	if len(user.Password) < 6 {
+		c.IndentedJSON(400, gin.H{"error": "password must be at least 6 characters long"})
+		c.Abort()
+		return
+	}
 
+	if len(user.Username) < 3 {
+		c.IndentedJSON(400, gin.H{"error": "username must be at least 3 characters long"})
+		c.Abort()
+		return
+	}
+
+	// Basic email validation 
+	emailRegex := `^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`
+	re := regexp.MustCompile(emailRegex)
+	match := re.MatchString(user.Email)
+	if !match {
+		c.IndentedJSON(400, gin.H{"error": "invalid email format"})
+		c.Abort()
+		return
+	}
+
+	if user.VCode == "" {
+		c.IndentedJSON(401, gin.H{"error": "insert verification code from your email"})
+		c.Abort()
+		return
+	}
+
+	err := uc.userUsecase.VerifyCode(ctx, user.Email, user.VCode)
+	if err != nil {
+		c.IndentedJSON(500, gin.H{"error": err.Error()})
+		c.Abort()
+		return
+	}
+
+	err = uc.userUsecase.Register(ctx, user)
 	if err != nil {
 		switch err.Error() {
-		case "username or email already exists":
+		case "username already exists":
+			fmt.Println("gb", "username")
+			c.IndentedJSON(409, gin.H{"error": err.Error()})
+		case "email already exists":
+			fmt.Println("email")
 			c.IndentedJSON(409, gin.H{"error": err.Error()})
 		default:
-			c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			c.IndentedJSON(500, gin.H{"error": err.Error()})
 		}
 
 		c.Abort()
 		return
 	}
 
-	c.IndentedJSON(http.StatusCreated, gin.H{"message": "user created successfully"})
+	err = uc.userUsecase.DeleteVCode(ctx, user.Email)
+	if err != nil {
+		c.IndentedJSON(500, gin.H{"error": err.Error()})
+		c.Abort()
+		return
+	}
+
+	c.IndentedJSON(200, gin.H{"message": "user created successfully"})
 }
+
+
