1c5614d1a320d83b3cb7b12edba722b31cd27eb2
Abreham Kassa <abrehamkassa19@gmail.com>
2025-08-06 11:19:48 +0300

    feat: define error constants

--- Domain/errors.go
+++ Domain/errors.go
@@ -7,13 +7,17 @@ import (
 var (
 
 	// General Errors
-	ErrInternalServerError   = errors.New("internal server error")
+	ErrInternalServer        = errors.New("internal server error")
 	ErrUserNotFound          = errors.New("user not found")
 	ErrUserAlreadyExist      = errors.New("user already exists")
 	ErrEmailAlreadyExists    = errors.New("email already exists")
 	ErrUnexpected            = errors.New("internal server error")
 	ErrUnauthorized          = errors.New("unauthorized")
 	ErrUsernameAlreadyExists = errors.New("username already exists")
-	ErrIncorrectUserID       = errors.New("incorrect userID")
+	ErrInvalidUserID         = errors.New("invalid userID")
 	ErrNoUpdate              = errors.New("no update has been applied")
+
+	// Token errors
+	ErrInvalidToken        = errors.New("invalid token")
+	ErrInvalidRefreshToken = errors.New("invalid refresh token")
 )
